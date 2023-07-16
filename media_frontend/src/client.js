89923496bfd1e0214e03b29b3a101464fe203abf
Mayukh Das <107027766+Mayukhy@users.noreply.github.com>
2023-07-16 22:52:04 +0530

    Update client.js

--- media_frontend/src/client.js
+++ media_frontend/src/client.js
@@ -3,11 +3,11 @@ import imageUrlBuilder from '@sanity/image-url';
 
 
 export const client = createClient({
-  projectId: 'ayhihq4k',
+  projectId: 'your ProjectId',
   dataset: 'mediaapp',
   apiVersion: '2021-10-21',
   useCdn: true,
-  token: 'skdgCJKeX8EV8Cdqdscs9bJcM5XOGX2XVNkgFVP0LuBzIsw1szu6lnTF4rMLBqxWC6UMt381vuXSFaVRe3qKjMPKYFxhEancdPtn1vnu5TZ8r86VzU78cOQi3AQCbyaDIsq89n76uPVhKJ9ufkmotYf5miR6Vwg8LGK0hkm5GQ1AIOA7Lv0I',
+  token: 'Your Token',
 });
 
 const builder = imageUrlBuilder(client);
